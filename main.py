a0cde5a704066de9d4e6147016979146ae1ac84b
Shornikov Vladislav <petrthefirstimp@gmail.com>
2023-06-02 17:22:46 +0300

    Задание 3.1

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 if __name__ == '__main__':
-    a = int(input("введите номер места "))
-    if a < 1 or a > 54:
-        print('Такого места нет')
-    elif a % 2 == 0 and a <= 36:
-        print('Вверхнее место в купе')
-    elif a % 2 != 0 and a <= 35:
-        print('Нижннее место в купе')
-    elif a % 2 == 0 and a > 36:
-        print('Вверхнее боковое место')
-    else:
-        print('Нижнее боковое место')
+    N = int(input())
+    i = 0
+    mess = ""
+    while i < N:
+        mess += input() + " "
+        i += 1
+    print(mess)
