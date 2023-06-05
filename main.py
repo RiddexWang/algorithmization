c6c7e5e8f935a1b43d9cdc3cd2d460f3ee7cf43c
Shornikov Vladislav <petrthefirstimp@gmail.com>
2023-06-05 21:37:13 +0300

    Задание 3.4

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def print_hi(name):
-    print(f'Hi, {name}')
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    import random
 
+    correct_answers = 0
+    incorrect_answers = 0
+
+    while incorrect_answers < 3:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        answer = input(f"{a} + {b} = ")
+        if int(answer) == a + b:
+            correct_answers += 1
+            print("Правильно!")
+        else:
+            incorrect_answers += 1
+            print("Ответ неверный")
+
+    print(f"Игра окончена. Правильных ответов: {correct_answers}")
