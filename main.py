1a55d329619d6fb44e4ef2ba6810bacca47e7a97
Shornikov Vladislav <petrthefirstimp@gmail.com>
2023-06-05 20:56:38 +0300

    Задание 2.4

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 if __name__ == '__main__':
-    i = 0
-    mess = ""
-    while i < 1:
-        word = input()
-        if word == 'stop':
-            break
-        else:
-             mess += word + " "
-    print(mess)
+    color1 = input("Введите первый цвет: ")
+    color2 = input("Введите второй цвет: ")
+
+    if (color1 == "Красный" and color2 == "Синий") or (color1 == "Синий" and color2 == "Красный"):
+        print("Фиолетовый")
+    elif (color1 == "Красный" and color2 == "Желтый") or (color1 == "Желтый" and color2 == "Красный"):
+        print("Оранжевый")
+    elif (color1 == "Синий" and color2 == "Желтый") or (color1 == "Желтый" and color2 == "Синий"):
+        print("Зеленый")
+    else:
+        print("Ошибка! Введите название одного из основных цветов: ККрасный, Синий или Желтый.")
