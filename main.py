b50ecd8fc6c0e729cd1dc69fb42d282a68283fa2
Shornikov Vladislav <petrthefirstimp@gmail.com>
2023-06-05 21:45:39 +0300

    Задание 3.3

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 if __name__ == '__main__':
-    import random
-
-    correct_answers = 0
-    incorrect_answers = 0
-
-    while incorrect_answers < 3:
-        a = random.randint(1, 10)
-        b = random.randint(1, 10)
-        answer = input(f"{a} + {b} = ")
-        if int(answer) == a + b:
-            correct_answers += 1
-            print("Правильно!")
+    while True:
+        word = input("Введите слово: ")
+        if word.lower() == "стоп":
+            break
+        elif "ф" in word.lower():
+            print("Ого! Это редкое слово!")
         else:
-            incorrect_answers += 1
-            print("Ответ неверный")
-
-    print(f"Игра окончена. Правильных ответов: {correct_answers}")
+            print("Эх, это не очень редкое слово...")
